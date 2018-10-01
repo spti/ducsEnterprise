317e43efa28589eb20c0704103bd66796479bebe
spti <dannvx@gmail.com>
2018-10-01 04:04:21 +0300

    remove redundant files

--- src/scripts/script.js
+++
@@ -1,9 +0,0 @@
-// import {Switcher} from '../components/switcher.js'
-import {App} from './home.js'
-
-function main() {
-  const container = document.querySelector('main')
-  ReactDOM.render(<App />, container) // <Switcher className="slider" visibleItems={3} items={data.items} />
-}
-
-export {main}
